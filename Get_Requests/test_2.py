80128976177a4ff626d81fe985716443eee64c0e
sg64840 <sg684840@gmail.com>
2023-01-14 22:46:45 +0530

    Adding second file for get api testing

---
+++ Get_Requests/test_2.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import pandas as pd
+
+search_api_url = 'https://www.boredapi.com/api/activity'
+response_1 = requests.get(search_api_url)
+print(response_1.status_code)
+if response_1.status_code==200 :
+    print(response_1.json())
+    response_2 = requests.get(search_api_url)
+    print(response_2.status_code)
+    if response_2.status_code==200 :
+        print(response_2.json())
+        if str(response_1.json())==str(response_2.json()) :
+            print("True")
+        else:
+            print("False")
+    else :
+        print("Response 2 error :", response_2.status_code)
+else :
+    print("Response 1 error :", response_1.status_code)
+
+
+
